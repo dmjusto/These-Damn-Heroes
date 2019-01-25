115307adf032a2bc33c93d9ff5b749433eabb036
Dylan <dylanfairbanks@icloud.com>
2019-01-25 09:42:40 -0700

    made RoomTransition.cs more readable

--- Assets/Scripts/RoomTransition.cs
+++ Assets/Scripts/RoomTransition.cs
@@ -15,7 +15,7 @@ public class RoomTransition : MonoBehaviour
   }
   public DoorType doorType;
   private BoxCollider2D doorTrigger;
-  private Camera camera;
+  private Transform cameraTransform;
   private GameObject player;
   public float playerDisplacement;
 
@@ -24,14 +24,8 @@ public class RoomTransition : MonoBehaviour
   {
     doorTrigger = gameObject.GetComponent<BoxCollider2D>();
     doorTrigger.isTrigger = true;
-    camera = FindObjectOfType<Camera>();
+    cameraTransform = FindObjectOfType<Camera>().transform;
     player = GameObject.FindGameObjectWithTag("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () 
-  {
-		
 	}
 
   private void OnTriggerEnter2D(Collider2D collision)
@@ -45,31 +39,32 @@ public class RoomTransition : MonoBehaviour
   {
     Vector3 newPos = new Vector3();
     Vector3 newPlayerPos = new Vector3();
+    float camX = cameraTransform.position.x;
+    float camY = cameraTransform.position.y;
+    float camZ = cameraTransform.position.z;
+
     switch (doorType)
     {
       case DoorType.up:
-        newPos = new Vector3(camera.transform.position.x, camera.transform.position.y + gridSize.y,
-          camera.transform.position.z);
+        newPos = new Vector3(camX, camY + gridSize.y, camZ);
         newPlayerPos = new Vector3(player.transform.position.x, player.transform.position.y + playerDisplacement, player.transform.position.z);
         break;
+
       case DoorType.down:
-        newPos = new Vector3(camera.transform.position.x, camera.transform.position.y - gridSize.y,
-          camera.transform.position.z);
+        newPos = new Vector3(camX, camY - gridSize.y, camZ);
         newPlayerPos = new Vector3(player.transform.position.x, player.transform.position.y - playerDisplacement, player.transform.position.z);
 
         break;
       case DoorType.left:
-        newPos = new Vector3(camera.transform.position.x - gridSize.x, camera.transform.position.y,
-          camera.transform.position.z);
+        newPos = new Vector3(camX - gridSize.x, camY, camZ);
         newPlayerPos = new Vector3(player.transform.position.x - playerDisplacement, player.transform.position.y, player.transform.position.z);
 
         break;
       case DoorType.right:
-        newPos = new Vector3(camera.transform.position.x + gridSize.x, camera.transform.position.y,
-          camera.transform.position.z);
+        newPos = new Vector3(camX + gridSize.x, camY, camZ);
         newPlayerPos = new Vector3(player.transform.position.x + playerDisplacement, player.transform.position.y, player.transform.position.z);
-
         break;
+
       default:
         break;
     }
