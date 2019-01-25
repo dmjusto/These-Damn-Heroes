8cb667625e3795ed15a367f689153808d9774b69
Dylan <dylanfairbanks@icloud.com>
2019-01-25 14:22:23 -0700

    finished implementing the camera control for moving from room to room

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour 
+{
+  private Vector3 newPos = new Vector3();
+  public float smoothTime = 0.2f;
+  private Vector3 currentVelocity = Vector3.zero;
+
+  // Use this for initialization
+  void Start () 
+  {
+    newPos = transform.position;
+  }
+
+  private void LateUpdate()
+  {
+    transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothTime);
+  }
+
+  public void ChangeRooms(Vector3 position)
+  {
+    newPos = position;
+  }
+}
+
--- Assets/Scripts/RoomTransition.cs
+++ Assets/Scripts/RoomTransition.cs
@@ -18,14 +18,18 @@ public class RoomTransition : MonoBehaviour
   private Transform cameraTransform;
   private GameObject player;
   public float playerDisplacement;
+  private Vector3 newPos = new Vector3();
+  private CameraControl cameraScript;
 
-	// Use this for initialization
-	void Start () 
+  // Use this for initialization
+  void Start () 
   {
     doorTrigger = gameObject.GetComponent<BoxCollider2D>();
     doorTrigger.isTrigger = true;
     cameraTransform = Camera.main.transform;
     player = GameObject.FindGameObjectWithTag("Player");
+    newPos = cameraTransform.position;
+    cameraScript = Camera.main.GetComponent<CameraControl>();
 	}
 
   private void OnTriggerEnter2D(Collider2D collision)
@@ -37,7 +41,7 @@ public class RoomTransition : MonoBehaviour
    to the correct adjacent room*/
   private void ChangeRooms()
   {
-    Vector3 newPos = new Vector3();
+
     Vector3 newPlayerPos = new Vector3();
     float camX = cameraTransform.position.x;
     float camY = cameraTransform.position.y;
@@ -45,6 +49,9 @@ public class RoomTransition : MonoBehaviour
     float playerX = player.transform.position.x;
     float playerY = player.transform.position.y;
     float playerZ = player.transform.position.z;
+
+
+
 
     /* depending on which way the door is facing, 
      * we set target positions for the move*/
@@ -73,8 +80,8 @@ public class RoomTransition : MonoBehaviour
       default:
         break;
     }
-    //Moves the camera and player to new room positions
-    cameraTransform.position = newPos;
     player.transform.position = newPlayerPos;
+    cameraScript.ChangeRooms(newPos);
   }
+
 }
